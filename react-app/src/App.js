96a3dc96fc57cddf8fd86d900058ae1e39772737
Fabiola Villatoro <fabiolamariavillatoro@gmail.com>
2023-05-27 10:28:15 -0400

    handleDelete() added and fully functional

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,38 @@
 import './App.css';
 import Form from './Components/Form';
 import List from './Components/List';
+import React, {useState} from 'react';
+
+const initialList = [{sourceName: "Presbo", sourceEmail: "presbo@columbia.edu"},
+                    {sourceName: "John Jay Mouse", sourceEmail: "mouse@columbia.edu"}, 
+                    {sourceName: "Water Bottle Man", sourceEmail: "flipper@columbia.edu"}]
 
 function App() {
+
+  const [sourceList, setSourceList] = useState(initialList);
+  //const refers to this two-item array, but not necessarily the vars within
+  //sourceList we are modifying!
+
+  //two methods here
+
+  function handleDelete(sourceIndex){
+    const updatedList = sourceList.slice(0, sourceIndex).concat(sourceList.slice(sourceIndex+1));
+
+    setSourceList(updatedList);
+  }
+  
+  function handleAdd(){
+
+  }
+
   return (
     <div class="App">
       <div><img src="https://clubs-cu.s3.amazonaws.com/Spectator+Publishing+Logo.png " class="logo"></img></div>
     <h1 class="heading">Spectator's Sources</h1>
     <Form></Form>
 
-    <List></List>
+    <List sourceList ={sourceList} handleDelete = {handleDelete}></List>
+
     </div>
   );
 }
--- react-app/src/Components/List.js
+++ react-app/src/Components/List.js
@@ -1,12 +1,8 @@
-const sourceList = [{sourceName: "Presbo", sourceEmail: "presbo@columbia.edu"},
-                    {sourceName: "John Jay Mouse", sourceEmail: "mouse@columbia.edu"}, 
-                    {sourceName: "Water Bottle Man", sourceEmail: "flipper@columbia.edu"}]
-
-function List(){
+function List({sourceList, handleDelete}){
     return(
     <>
     {sourceList.map((source, index) => (
-        <ListItem sourceId={index+1} sourceName={source.sourceName} sourceEmail={source.sourceEmail}/>
+        <ListItem sourceId={index} sourceName={source.sourceName} sourceEmail={source.sourceEmail} handleDelete={handleDelete}/>
     ))}
     </>
     );
@@ -14,13 +10,13 @@ function List(){
 
 export default List;
 
-function ListItem({sourceId, sourceName, sourceEmail}){
+function ListItem({sourceId, sourceName, sourceEmail, handleDelete}){
     return(
         <div class="entry">
-        <div class="item"><p class="entry-text">{sourceId}</p></div>
+        <div class="item"><p class="entry-text">{sourceId+1}</p></div>
         <div class="item"><p class="entry-text">{sourceName}</p></div>
         <div><p class="entry-email">{sourceEmail}</p></div>
-        <button class="deleteButton"><p class="button-text">DELETE</p></button>
+        <button class="deleteButton" onClick={() => handleDelete(sourceId)}><p class="button-text">DELETE</p></button>
         </div>
     );
 }
