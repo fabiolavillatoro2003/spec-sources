cf6108cbf112ee8f1587796d18b4e0b7bfa721ad
Fabiola Villatoro <fabiolamariavillatoro@gmail.com>
2023-05-26 23:47:09 -0400

    Created ListItem JavaScript component. Ready to introduce props.

--- react-app/src/Components/List.js
+++ react-app/src/Components/List.js
@@ -1,25 +1,22 @@
 function List(){
     return(
-    <div>
-    <div class="entry">
+    <>
+    <ListItem></ListItem>
+    <ListItem></ListItem>
+    <ListItem></ListItem>
+    </>
+    );
+}
+
+export default List;
+
+function ListItem(){
+    return(
+        <div class="entry">
         <div class="item"><p class="entry-text">1</p></div>
         <div class="item"><p class="entry-text">Presbo</p></div>
         <div><p class="entry-email">presbo@columbia.edu</p></div>
         <button class="deleteButton"><p class="button-text">DELETE</p></button>
-    </div>
-    <div class="entry">
-        <div class="item"><p class="entry-text">2</p></div>
-        <div class="item"><p class="entry-text">John Jay Mouse</p></div>
-        <div class="item"><p class="entry-email">mouse@columbia.edu</p></div>
-        <button class="deleteButton"><p class="button-text">DELETE</p></button>
-    </div>
-    <div class="entry">
-        <div class="item"><p class="entry-text">1</p></div>
-        <div class="item"><p class="entry-text">Water Bottle Man</p></div>
-        <div class="item"><p class="entry-email">flipper@columbia.edu</p></div>
-        <button class="deleteButton"><p class="button-text">DELETE</p></button>
-    </div>
-    </div>
+        </div>
     );
 }
-export default List;
