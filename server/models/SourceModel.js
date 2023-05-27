be6342bb97d686249adba6e1144e06f644a7d0c6
Fabiola Villatoro <fabiolamariavillatoro@gmail.com>
2023-05-27 14:44:15 -0400

    MongoDB set up, trying to finalize work towards making successful fetch requests.

---
+++ server/models/SourceModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//WRITE THE SCHEMA HERE
+
+const sourceSchema = new Schema({
+    sourceId: {type: String, required: true},
+    sourceName: {type: String, required: true},
+    sourceEmail: {type: String, required: true},
+})
+//A SCHEMA defines one single thing
+
+//A MODEL is like a constructor
+const sources = mongoose.model('spec_sources', sourceSchema);
+
+module.exports = sources;
+
---
+++ server/models/config.js
@@ -0,0 +1,12 @@
+require('dotenv').config({path: path.resolve(__dirname,'../../.env')});
+
+const dbUser = process.env.DBUSER;
+const dbName = process.env.DBNAME;
+const dbPassword = process.env.DBPASSWORD;
+const discardAfterXDays = 14;
+const URI = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.ornlmee.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+module.exports = {
+    URI: URI,
+    discardAfterXDays: discardAfterXDays
+};
