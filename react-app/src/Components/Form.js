03ca417c3d034a24ace1e7647363251cc0980bee
Fabiola Villatoro <fabiolamariavillatoro@gmail.com>
2023-05-26 21:55:31 -0400

    Deleted a lot, appears to be going backwards but we are actually making progress. Had to unravel some spaghetti code. Nav bar 90% done.

--- react-app/src/Components/Form.js
+++ react-app/src/Components/Form.js
@@ -1,13 +1,18 @@
 function Form(){
     return(
+        <div>
     <div class="nav">
-        <p class="nav-text">Source Name:</p>
+        <p id="nav-text">Source Name:</p>
         <input type="text" id="name" name="name" required></input>
-        <p class="nav-text">Source Email:</p>
+        <p id="nav-text">Source Email:</p>
         <input type="text" id="email" name="email" required></input>
         <button class="addButton"><p class="button-text">ADD</p></button>
+    
     </div>
-
+    <div class="test">
+        
+    </div>
+    </div>
     );
 }
 export default Form;
